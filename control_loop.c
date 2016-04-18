48fe5c3f6d7ae5ff979d783174ed5e6dfe40f24c
Grant Boesiger <boesigerg@msoe.edu>
2016-04-18 18:56:10 -0500

    Added tilt sensor function to detect over-tilt

--- control_loop.c
+++ control_loop.c
@@ -2,15 +2,21 @@
 #include <SoftwareSerial.h>   //Only necessary if not using hardware serial pins
 #include <math.h>
 
+//Attach GPS power to 5V
 #define GPS_TX 3          //GPS transmit connected to digital pin 3
 #define GPS_RX 2          //GPS read connected to digital pin 2
 #define GPS_BAUD 9600     //GPS Baud rate
 
+//Attach Prox power to 5V
 #define PROX1 A0          //
 #define PROX2 A1          //
 #define PROX3 A2          //Proximity Sensor Array
 #define PROX4 A3          //
 #define PROX5 A4          //
+
+//Attach Accel power to 3.3V
+#define TILTX A5          //Tilt Sensor X Axis
+#define TILTY A6          //Tilt Sensor Y Axis (Pin doesnt exist)
 
 float gpsLong, gpsLat;                    //GPS longitude/latitude in degrees
 uint8_t gpsHour, gpsMin, gpsSec;          //GPS data time stamp
@@ -19,6 +25,8 @@ float gpsAngle;                           //Mower angle heading (Check if float)
 byte PROX_ARRAY[] = {PROX1, PROX2, PROX3,
                          PROX4, PROX5};   //Proximity Pin Array
 uint8_t prox[5];                          //Proximity Sensor Array Values
+
+int accelX, accelY;                       //Tilt Sensor output values
 
 uint32_t timer = millis();
 
@@ -31,7 +39,7 @@ SIGNAL(TIMER0_COMPA_vect) {
 
 void setup() {
 
-  //Serial.begin(115200);
+  Serial.begin(115200);
 
   GPS.begin(GPS_BAUD);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);    //Minimum recommended data output
@@ -53,6 +61,9 @@ void loop() {
 
   updateProxData();
   updateGPSData();
+  readTiltSensor();
+
+  printData();
 
   delay(500);
 }
@@ -75,6 +86,31 @@ void updateProxData(){
     for(j=0;j<10;j++){
       senseVal+=analogRead(PROX_ARRAY[i]);
     }
-    prox[i] = (uint8_t) 12878*pow((senseVal/10),-1.179);
+    prox[i] = 12878*pow((senseVal/10),-1.179);
   }
+}
+
+void readTiltSensor(){
+  accelX = analogRead(TILTX);
+  accelY = analogRead(TILTY);
+  if(accelX>370 || accelX<300 || accelY>375 || accelY<305){
+    //IMMEDIATE MOTOR STOP
+    //Set status to stopped
+  }
+}
+
+void printData(){
+  Serial.print("GPS Time: ");
+  Serial.print(gpsHour, DEC); Serial.print(':');
+  Serial.print(gpsMin, DEC); Serial.print(':');
+  Serial.println(gpsSec, DEC);
+  Serial.print("GPS Data: ");
+  Serial.print(gpsLat,6);Serial.print(", ");
+  Serial.println(gpsLong,6);
+  Serial.print("Prox Data: ");
+  Serial.print(prox[0]);Serial.print("\t");
+  Serial.print(prox[1]);Serial.print("\t");
+  Serial.print(prox[2]);Serial.print("\t");
+  Serial.print(prox[3]);Serial.print("\t");
+  Serial.println(prox[4]);
 }
